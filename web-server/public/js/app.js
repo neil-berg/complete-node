fbcc56263f4ab6cce9ec9ef12810d9a26f808ebe
Neil Berg <neil.berg14@gmail.com>
2019-07-26 19:30:38 -0700

    initial commit

---
+++ web-server/public/js/app.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector('form');
+const responseMessage = document.querySelector('.response-message');
+
+const fetchForecast = async address => {
+  const url = await `http://localhost:3000/weather?address=${address}`;
+  try {
+    responseMessage.textContent = `Finding forecast for ${address}...`;
+    const res = await fetch(url);
+    const data = await res.json();
+    responseMessage.textContent = data.summary;
+  } catch (error) {
+    responseMessage.textContent = error;
+  }
+};
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const searchInput = document.querySelector('input');
+  const address = searchInput.value;
+  if (address) {
+    fetchForecast(address);
+  } else {
+    responseMessage.textContent = 'Please enter a valid address';
+  }
+});
